8e8f0292773d51a7c0fdc0437a954fc09f141ccb
AlfieLumley <122247757+AlfieLumley@users.noreply.github.com>
2023-08-27 20:13:13 +0100

    Add files via upload

---
+++ PythonDices.py
@@ -0,0 +1,14 @@
+import random
+
+dice=[1,2,3,4,5,6]
+roll_again="yes"
+while roll_again =="yes" or roll_again =="y":
+    print("Rolling the dices...")
+    print("The values are")
+    value1=random.choice(dice)
+    value2=random.choice(dice)
+    print(value1,value2)
+    roll_again = input ("Type 'y' or 'yes' to roll again:")
+
+print("Thanks for playing!")
+                      
